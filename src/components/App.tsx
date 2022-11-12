ce163a7343832e449ba5ec16f4656d9e28979062
Igor Trentini <igortrentini.2004@gmail.com>
2022-11-12 16:14:22 -0300

    feat: Configuracoes de estilo

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Button } from "./ui/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../styles/themes/default";
+import { GlobalStyle } from "../styles/global";
+
+export const App: FC = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="success">salve</Button>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
---
+++ src/components/ui/Button/index.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from "react";
+import { ButtonContainer, ButtonVariant } from "./index.styles";
+
+interface ButtonProps {
+  children: ReactNode;
+  variant?: ButtonVariant;
+}
+
+export const Button: FC<ButtonProps> = ({ children, variant = "primary" }) => {
+  return <ButtonContainer variant={variant}>{children}</ButtonContainer>;
+};

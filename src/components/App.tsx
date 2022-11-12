54563b20c8ba17e46dda4321fb631b85274c7097
Igor Trentini <igortrentini.2004@gmail.com>
2022-11-12 16:22:32 -0300

    feat: Eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
-import { Button } from "./ui/Button";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "../styles/themes/default";
-import { GlobalStyle } from "../styles/global";
+import { FC } from 'react'
+import { Button } from './ui/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+import { GlobalStyle } from '../styles/global'
 
 export const App: FC = () => {
   return (
@@ -11,5 +11,5 @@ export const App: FC = () => {
 
       <GlobalStyle />
     </ThemeProvider>
-  );
-};
+  )
+}
--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,11 +1,11 @@
-import { FC, ReactNode } from "react";
-import { ButtonContainer, ButtonVariant } from "./index.styles";
+import { FC, ReactNode } from 'react'
+import { ButtonContainer, ButtonVariant } from './index.styles'
 
 interface ButtonProps {
-  children: ReactNode;
-  variant?: ButtonVariant;
+  children: ReactNode
+  variant?: ButtonVariant
 }
 
-export const Button: FC<ButtonProps> = ({ children, variant = "primary" }) => {
-  return <ButtonContainer variant={variant}>{children}</ButtonContainer>;
-};
+export const Button: FC<ButtonProps> = ({ children, variant = 'primary' }) => {
+  return <ButtonContainer variant={variant}>{children}</ButtonContainer>
+}

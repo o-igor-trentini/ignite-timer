7a9bbfec9effddf38d2a9fcbca07dca5ae35dd52
Igor Trentini <igortrentini.2004@gmail.com>
2022-11-16 20:31:56 -0300

    feat: Aprimorado input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,20 +13,29 @@ import {
 export const Home: FC = () => {
   return (
     <HomeContainer>
-      <form action="">
+      <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="projeto 1" />
+            <option value="projeto 2" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            min={5}
+            max={240}
+            step={5}
           />
 
           <span>minutos.</span>
